43ac4725f8ce9a284edfe694092bec155adbc1df
AshutoshSingh058 <147975384+AshutoshSingh058@users.noreply.github.com>
2025-01-23 15:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (24.22%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,46 +10,16 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* curr=head ,* P=NULL, *N=NULL;
-        while(curr ){
-            N=curr->next;
-            curr->next=P;
-            P=curr;
-            curr=N;
-        }
-        // if(curr!=NULL)curr->next=P;
-        return P;
-    }
-
-    // ListNode* reverseList(ListNode* head) {
-        
-    //     if(head==NULL || head->next==NULL) return head;
-
-    //     ListNode* newHead= reverseList(head->next);
-
-    //     ListNode* front= head->next;
-    //     front->next= head;
-    //     head->next=NULL;
-
-    //     return newHead;
-    // }
-
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(!head || !head->next)return nullptr;
+        ListNode* slow = head; ListNode* fast = head;
+        for(int i=0; i<n; i++)fast=fast->next;
+        if(!fast)return (head->next);
 
-        head = reverseList(head);
-        ListNode* temp = head;
-        if(n==1){
-            return reverseList(head->next);
+        while(fast->next){
+            fast=fast->next;
+            slow = slow->next;
         }
-        while(n--){
-            if(n==1)temp->next=temp->next->next;
-            if(temp)temp=temp->next;
-        }
-        
-        head = reverseList(head);
-
+        slow->next=slow->next->next;
         return head;
     }
 };
