d3f189fa90c066b580be2474e280d20a765254da
AshutoshSingh058 <147975384+AshutoshSingh058@users.noreply.github.com>
2025-01-24 10:25:10 +0530

    Time: 54 ms (20.56%) | Memory: 75.6 MB (35.30%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -1,80 +1,3 @@
-// /**
-//  * Definition for singly-linked list.
-//  * struct ListNode {
-//  *     int val;
-//  *     ListNode *next;
-//  *     ListNode() : val(0), next(nullptr) {}
-//  *     ListNode(int x) : val(x), next(nullptr) {}
-//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-//  * };
-//  */
-// class Solution {
-// public:
-
-//     ListNode* findMiddle(ListNode* head){
-//         if(!head || !head->next){
-//             return head;
-//         }
-//         ListNode* fast=head->next, *slow= head;
-
-//         while(!fast && !fast->next){
-//             fast=fast->next->next;
-//             slow= slow->next;
-//         }
-//         return slow;
-//     }
-
-//     ListNode* Merge(ListNode* leftnode, ListNode* rightnode){
-//         ListNode * dummyNode= new ListNode(-1);
-//         ListNode *ptr =dummyNode;
-//         while(leftnode && rightnode){
-//             if(leftnode->val <= rightnode->val){
-//                 ptr->next=leftnode;              
-//                 leftnode=leftnode->next;
-//             }
-//             else {
-//                 ptr->next=rightnode;
-//                 rightnode=rightnode->next;
-//             }
-//             ptr= ptr->next;
-//         }
-        
-//         if(leftnode){
-//             ptr->next=leftnode;
-//         }
-//         else {
-//             ptr->next= rightnode;
-//         }
-
-//         return (dummyNode->next);
-//     }
-
-//     // ListNode* MergeSort(ListNode* head){
-//     //     if(!head || !head->next)return head;
-//     //     ListNode* middle= findMiddle(head);
-//     //     ListNode *lefthead= head;
-//     //     ListNode *righthead = middle->next;
-//     //     middle->next=nullptr;
-//     //     lefthead=MergeSort(lefthead);
-//     //     righthead= MergeSort(righthead);
-//     //     return Merge(lefthead, righthead);
-//     // }
-
-//     ListNode* sortList(ListNode* head) {
-//         if(!head || !head->next)return head;
-
-//         ListNode* middle= findMiddle(head);
-//         ListNode *lefthead= head;
-//         ListNode *righthead = middle->next;
-//         middle->next=nullptr;
-//         lefthead=sortList(lefthead);
-//         righthead= sortList(righthead);
-//         return Merge(lefthead, righthead);
-
-//         return head;
-//     }
-// };
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -86,59 +9,61 @@
  * };
  */
 class Solution {
-    private:
-    // Helper to find the middle of the list
-    ListNode* getMiddle(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head->next;
+public:
 
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
+    ListNode* findMiddle(ListNode* head){
+        if(!head || !head->next){
+            return head;
         }
+        ListNode* fast=head->next, *slow= head;
 
+        while(fast && fast->next){
+            fast=fast->next->next;
+            slow= slow->next;
+        }
         return slow;
     }
 
-    ListNode* merge(ListNode* l1, ListNode* l2){
-        ListNode* dummy = new ListNode(0);
-        ListNode* current = dummy;
-
-        while(l1 && l2){
-            if(l1->val<=l2->val){
-                current->next=l1;
-                l1=l1->next;
-            }else{
-                current->next=l2;
-                l2=l2->next;
+    ListNode* Merge(ListNode* leftnode, ListNode* rightnode){
+        ListNode * dummyNode= new ListNode(-1);
+        ListNode *ptr =dummyNode;
+        while(leftnode && rightnode){
+            if(leftnode->val <= rightnode->val){
+                dummyNode->next=leftnode;              
+                leftnode=leftnode->next;
             }
-            current=current->next;
+            else {
+                dummyNode->next=rightnode;
+                rightnode=rightnode->next;
+            }
+            dummyNode= dummyNode->next;
         }
-        if(l1) current->next=l1;
-        if(l2) current->next=l2;
-        return dummy->next;
+        
+        if(leftnode){
+            dummyNode->next=leftnode;
+        }
+        else {
+            dummyNode->next= rightnode;
+        }
 
+        return (ptr->next);
     }
-public:
+
+    ListNode* MergeSort(ListNode* head){
+        if(!head || !head->next)return head;
+
+        ListNode* middle= findMiddle(head);
+        ListNode *lefthead= head;
+        ListNode *righthead = middle->next;
+        middle->next=nullptr;
+        lefthead=MergeSort(lefthead);
+        righthead= MergeSort(righthead);
+        return Merge(lefthead, righthead);
+    }
+
     ListNode* sortList(ListNode* head) {
-        /* Bruteforce:-
-        Extract all the values from the linked list into a vector/array.
-        Sort the vector/array using any efficient sorting algorithm.
-        Reconstruct the linked list using the sorted array.*/
+        head = MergeSort(head);
 
-        //Optimised using merge sort, first find mid, divide into two halves, sort botht eh halves and then merge 
-        if (!head || !head->next) return head;
-        ListNode* mid = getMiddle(head);
-        ListNode* left= head;
-        ListNode* right= mid->next;
-
-        mid->next = NULL;
-
-        left = sortList(left);
-        right = sortList(right);
-
-        // now lets merge
-        return merge(left,right);
-
+        return head;
     }
 };
