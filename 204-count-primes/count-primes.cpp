44816e31f7e0e9f2a08b2bdae466c3e210f4a085
AshutoshSingh058 <147975384+AshutoshSingh058@users.noreply.github.com>
2025-02-03 23:24:36 +0530

    Time: 159 ms (85.28%) | Memory: 12.6 MB (65.88%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -4,16 +4,16 @@ public:
         vector<bool> arr(n+1,1);
         arr[0]=arr[1]=0;
         int count =0;
-        for(int i=2; i<n;i++){
+        for(int i=2; i*i<n;i++){
             // arr[i]=-1;
-            if( !arr[i] )continue;
-            count++;
-            for(int j=i*2; j<n; j+=i){
+            if(arr[i] == 0)continue;
+            // count++;
+            for(int j=i*i; j<n; j+=i){
                 arr[j]=0;
             }
             
         }
-        // for(int i=0; i<=n;i++)cout<<arr[i]<<" ";
+        for(int i=0; i<n;i++)count+=arr[i];
         return count;
     }
 };
