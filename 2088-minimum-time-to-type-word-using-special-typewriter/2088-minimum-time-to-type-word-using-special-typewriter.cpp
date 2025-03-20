fb7aefcb663ba7dadaa406629bc35df49e129568
AshutoshSingh058 <147975384+AshutoshSingh058@users.noreply.github.com>
2025-03-20 13:09:54 +0530

    Time: 0 ms (100%), Space: 8.1 MB (91.97%) - LeetHub

--- 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.cpp
+++ 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int minTimeToType(string word) {
-        int n = word.size(), c=n, k=0;
-        c+=min(min(abs(int('a'-word[0])), abs(26-int('a'-word[0]))),min(abs(int(word[0]-'a')), abs(26-int(word[0]-'a'))));
-        // cout<<abs(int('a'-word[0]))<<" "<<abs(26-int('a'-word[0]))<<" "<<abs(int(word[0]-'a'))<<" "<<abs(26-int(word[0]-'a'));
-        cout<<int(word[0]-'a')<<" "<<c<<endl;
-        for(int i=1; i<n; i++){
-            c+=min(min(abs(int(word[i-1]-word[i])), abs(26-int(word[i-1]-word[i]))),min(abs(int(word[i]-word[i-1])), abs(26-int(word[i]-word[i-1]))));
-            cout<<c<<endl;
+        int sol=0;
+        char current = 'a';
+        for(char c  : word){
+            int direct_distance = abs( c- current);
+            int anit_distance  = 26-direct_distance;
+            sol += min(direct_distance, anit_distance) + 1; //type + move
+            current = c;
         }
-        return c;
+        return sol;
+            
     }
 };
