433783ce1dbf5197d312f2b64cfbe316ecc867f0
Ashutosh Singh <147975384+AshutoshSingh058@users.noreply.github.com>
2025-12-31 11:59:34 +0530

    Time: 0 ms (100%), Space: 10.3 MB (70.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int prev2 = 0, prev1= nums[0], curr =0;
-        int p =0, np =0;
+        // int p =0, np =0;
         for(int i=1; i<nums.size(); i++){
-            p = nums[i] + prev2 ;
-            np = prev1;
-            curr = max(p,np);
+            // p = nums[i] + prev2 ;
+            // np = prev1;
+            curr = max(nums[i] + prev2 ,prev1);
             // cout<< prev2 << prev1 << curr <<endl;
             prev2= prev1;
             prev1= curr;
