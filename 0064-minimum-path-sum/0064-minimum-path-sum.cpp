5177cdbdd13129cd2638127112e63e697244ec9b
Ashutosh Singh <147975384+AshutoshSingh058@users.noreply.github.com>
2026-01-05 16:15:38 +0530

    Time: 0 ms (100%), Space: 16 MB (70.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -16,8 +16,43 @@ public:
 
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n,-1));
+        // vector<vector<int>> dp(m, vector<int>(n,-1));     
+        // return func(m-1 ,n-1 , grid, dp );
+        
+        // tabulation
+        // vector<vector<int>> dp(m, vector<int>(n,-1));     
 
-        return func(m-1 ,n-1 , grid, dp );
+        // for (int i=0; i<m; i++) {
+
+        //     for(int j=0; j<n; j++) {
+        //         if(i==0 && j==0) dp[i][j] = grid[i][j];
+        //         else {
+        //             int up = 1000001, left = 1000001;
+        //             if( i > 0) up = grid[i][j] + dp[i-1][j];
+        //             if (j > 0) left = grid[i][j]  + dp[i][j-1];
+
+        //             dp[i][j] = min(up, left);
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        // Space Optimization
+        vector<int> prev(n, 0);
+        for (int i=0; i<m; i++) {
+            vector<int> temp(n,0);
+            for(int j=0; j<n; j++) {
+                if(i==0 && j==0) temp[j] = grid[i][j];
+                else {
+                    int up = 1000001, left = 1000001;
+                    if( i > 0) up = grid[i][j] + prev[j];
+                    if (j > 0) left = grid[i][j]  + temp[j-1];
+
+                    temp[j] = min(up, left);
+                }
+            }
+            prev = temp;
+        }
+        return prev[n-1];
     }
 };
