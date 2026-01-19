1d2cac19910fac52371de422916b9ad33fd1092e
Ashutosh Singh <147975384+AshutoshSingh058@users.noreply.github.com>
2026-01-19 19:20:52 +0530

    Time: 197 ms (81.11%), Space: 87.8 MB (29.24%) - LeetHub

--- 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.cpp
+++ 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.cpp
@@ -3,19 +3,16 @@ public:
     
     int centeredSubarrays(vector<int>& nums) {
         int n = nums.size(), tot=0, s=0;
-        map<int, int> mp;
 
         for(int i=0; i<n; i++){
             s = 0;
+            map<int, int> mp;
             for(int j=i; j<n; j++){
                 s+= nums[j];
+                
+                mp[nums[j]]++;
+                if(mp.find(s)!= mp.end()) tot++;
 
-                for(int k=i; k<=j; k++){
-                    if(s == nums[k]) {
-                        tot++;
-                        break;
-                    }
-                }
             }
         }
         return tot;
