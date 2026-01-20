c58b50ab1688a8b5cc9dc1577e4270696a694184
Ashutosh Singh <147975384+AshutoshSingh058@users.noreply.github.com>
2026-01-20 10:09:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (89.42%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,16 +1,28 @@
+// class Solution {
+// public:
+//     int func( int n,  vector<int>& v){
+//         if (n<=1) return 1;
+//         if(v[n]!=-1) return v[n];
+//         int ans =0;
+//         for( int i=1; i<=n; i++){
+//             ans += func(i-1, v)*func(n-i, v);
+//         }
+//         return ans;
+//     }
+//     int numTrees(int n) {
+//         vector<int> v(n+1, -1);
+//         return func(n, v);
+//     }
+// };
+
 class Solution {
 public:
-    int func( int n,  vector<int>& v){
-        if (n<=1) return 1;
-        if(v[n]!=-1) return v[n];
-        int ans =0;
-        for( int i=1; i<=n; i++){
-            ans += func(i-1, v)*func(n-i, v);
-        }
-        return ans;
-    }
+    int dp[20]{};
     int numTrees(int n) {
-        vector<int> v(n+1, -1);
-        return func(n, v);
+        if(n <= 1) return 1;
+        if(dp[n]) return dp[n];
+        for(int i = 1; i <= n; i++) 
+            dp[n] += numTrees(i-1) * numTrees(n-i);
+        return dp[n];
     }
 };
