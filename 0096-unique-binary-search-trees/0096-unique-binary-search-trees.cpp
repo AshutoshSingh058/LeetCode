35b60bedb5d524c422392a2e4d0870189ba42fbd
Ashutosh Singh <147975384+AshutoshSingh058@users.noreply.github.com>
2026-01-20 10:04:04 +0530

    Time: 2538 ms (5.1%), Space: 7.9 MB (62.43%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    int numTrees(int n) {
+    int func( int n,  vector<int>& v){
         if (n<=1) return 1;
+        if(v[n]!=-1) return v[n];
         int ans =0;
         for( int i=1; i<=n; i++){
-            ans += numTrees(i-1)*numTrees(n-i);
+            ans += func(i-1, v)*func(n-i, v);
         }
         return ans;
+    }
+    int numTrees(int n) {
+        vector<int> v(n+1, -1);
+        return func(n, v);
     }
 };
