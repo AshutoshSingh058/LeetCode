7fb2c846bf4185ff9c23c5614149d81e3f133d8d
Ashutosh Singh <147975384+AshutoshSingh058@users.noreply.github.com>
2026-01-23 00:33:37 +0530

    Time: 0 ms (100%), Space: 9.6 MB (5.7%) - LeetHub

--- 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     string reversePrefix(string s, int k) {
         string ss = s.substr(0, k);
-        cout<<ss <<" " <<endl;
+        // cout<<ss <<" " <<endl;
         reverse(ss.begin(), ss.end());
         string ss2 = s.substr(k, s.size()-k);
-        cout<<ss <<" " << ss2<<endl;
-        string st = ss + ss2;
+        // cout<<ss <<" " << ss2<<endl;
+        string st = ss + s.substr(k, s.size()-k);
 
         return st;
     }
