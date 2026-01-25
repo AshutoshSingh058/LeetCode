26eff925fd8c6f0f7efb7a2d3869b9c8e7002d8d
Ashutosh Singh <147975384+AshutoshSingh058@users.noreply.github.com>
2026-01-25 10:30:36 +0530

    Time: 2 ms (7.63%), Space: 10.7 MB (44.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,11 +15,30 @@ public:
     }
 
 
+    // int rob(vector<int>& nums) {
+    //     int sum =0, maxSum = 0, n = nums.size();
+    //     //memoization
+    //     vector<int> dp(n,-1);
+    //     maxSum = func(n-1, nums, dp, 0);
+    //     return maxSum;
+    // }
+
     int rob(vector<int>& nums) {
         int sum =0, maxSum = 0, n = nums.size();
-        //memoization
+        //tabulation
         vector<int> dp(n,-1);
-        maxSum = func(n-1, nums, dp, 0);
-        return maxSum;
+        // dp[0] = nums[0];
+        for(int i=0; i<n; i++){
+
+            int pick =nums[i], notpick =0;
+            if(i-2 >=0) pick = nums[i] + dp[i-2] ;
+            if(i-1 >=0) notpick = dp[i-1] ;
+
+            dp[i] = max(pick, notpick);
+        }
+        // maxSum = func(n-1, nums, dp, 0);
+        // dp[n-1] = max(dp[n-1], dp[n-2]);
+        for(int i=0; i<n; i++) cout<<dp[i]<<" ";
+        return dp[n-1];
     }
 };
