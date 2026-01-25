cd0a7297b027286381aac3f92776b6ecada23c25
Ashutosh Singh <147975384+AshutoshSingh058@users.noreply.github.com>
2026-01-25 10:33:16 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -25,20 +25,27 @@ public:
 
     int rob(vector<int>& nums) {
         int sum =0, maxSum = 0, n = nums.size();
-        //tabulation
-        vector<int> dp(n,-1);
+        //tabulation 
+        // vector<int> dp(n,-1);
+        //sp opt
+        int n1 = 0, n2 = 0;
         // dp[0] = nums[0];
         for(int i=0; i<n; i++){
 
             int pick =nums[i], notpick =0;
-            if(i-2 >=0) pick = nums[i] + dp[i-2] ;
-            if(i-1 >=0) notpick = dp[i-1] ;
+            // if(i-2 >=0) pick = nums[i] + dp[i-2] ;
+            // if(i-1 >=0) notpick = dp[i-1] 
+            ;
+            if(i-2 >=0) pick = nums[i] + n2 ;
+            if(i-1 >=0) notpick = n1 ;
 
-            dp[i] = max(pick, notpick);
+            n2 = n1;
+            n1 = max(pick, notpick);
         }
         // maxSum = func(n-1, nums, dp, 0);
         // dp[n-1] = max(dp[n-1], dp[n-2]);
-        for(int i=0; i<n; i++) cout<<dp[i]<<" ";
-        return dp[n-1];
+        // for(int i=0; i<n; i++) cout<<dp[i]<<" ";
+        // return dp[n-1];
+        return n1;
     }
 };
