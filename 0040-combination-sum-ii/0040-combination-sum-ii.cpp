ceb90a10a83319cecb1e536a2daf005cc58b309a
Ashutosh Singh <147975384+AshutoshSingh058@users.noreply.github.com>
2026-02-21 13:42:21 +0530

    Time: 3 ms (39.86%), Space: 13.9 MB (82.93%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    void func(vector<int>& candidates, int target, int sum, vector<int>v, 
-    set<vector<int>>&vec, int start){
-        if(sum == target){
-            vec.insert(v);
-        }
-        for(int i=start; i<candidates.size(); i++){
-            if (i > start && candidates[i] == candidates[i - 1])
-                continue;
+    int n=0;
 
-            // Break early since array is sorted
-            if (candidates[i] > target)
-                break;
-                
-            if(sum + candidates[i] <= target){
-                v.push_back(candidates[i]);
-                func(candidates, target, sum + candidates[i], v, vec, i+1);
-                v.pop_back();
-            }
+    void func(vector<int>& candidates,int &target, int start, int sum, vector<int> &v, vector<vector<int>>&ans){
+        if( sum > target) return;
+        if(sum == target){
+            // sort(v.begin(), v.end());
+            ans.push_back(v);
+            return;
         }
+
+        for(int i=start; i<n; i++){
+            if(i> start && candidates[i] == candidates[i-1])continue;
+            v.push_back(candidates[i]);
+            func(candidates, target, i+1, sum + candidates[i], v, ans);
+            v.pop_back();
+        }
+
+        return;
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        set<vector<int>>vec;
-        vector<vector<int>>v2;
-        int sum =0;
-        vector<int>v;
+        vector<vector<int>>ans;
+        vector<int> v;
         sort(candidates.begin(), candidates.end());
-        func(candidates, target, sum, v , vec , 0 );
-        for(auto &i: vec){
-            v2.push_back(i);
-        }
-        return v2;
+        n = candidates.size(); 
+        func(candidates, target, 0, 0, v, ans);
+
+        return ans;
     }
 };
